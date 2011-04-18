d338fc6b35d3632f339206f2c30130f733693c09
pkarasev3 <karasevpa@gmail.com>
2011-04-17 22:16:53 -0400

    update script to properly check for files, run on two test images. runs in about 15 seconds to good naswer. ..

--- testdata/run_rigid_template_image_test.sh
+++ testdata/run_rigid_template_image_test.sh
@@ -1,9 +1,12 @@
-
-if [ -a "image_0000.jpg"]; then
+testfile='./image_0000.jpg'
+if [ -f  ${testfile} ]
+then
   echo "images already exist, not downloading"
 else
   wget http://www.theveganrobot.com/nlmagick/data/image_0000.jpg
   wget http://www.theveganrobot.com/nlmagick/data/image_0001.jpg
+  wget http://www.theveganrobot.com/nlmagick/data/image_0002.jpg
   wget http://www.theveganrobot.com/nlmagick/data/image_mask.jpg
 fi
-../build.debug/bin/template_nlopt_rigid   -i image_0001.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA
+../build.debug/bin/template_nlopt_rigid   -i image_0001.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA -w warped01.jpg
+../build.debug/bin/template_nlopt_rigid   -i image_0002.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA -w warped02.jpg
