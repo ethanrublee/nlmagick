c6f7e3f1e429da2808e3f75d1f72a23123a5c09d
pkarasev <pkarasev@gatech.edu>
2011-04-22 14:32:42 -0400

    fix script to have correct name

--- testdata/run_rigid_template_image_test.sh
+++ testdata/run_rigid_template_image_test.sh
@@ -8,5 +8,5 @@ else
   wget http://www.theveganrobot.com/nlmagick/data/image_0002.jpg
   wget http://www.theveganrobot.com/nlmagick/data/image_mask.jpg
 fi
-../build.debug/bin/template_nlopt_rigid   -i image_0001.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA -w warped01.jpg
-../build.debug/bin/template_nlopt_rigid   -i image_0002.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA -w warped02.jpg
+../build.debug/bin/template_image_rigid   -i image_0001.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA -w warped01.jpg
+../build.debug/bin/template_image_rigid   -i image_0002.jpg  -m image_mask.jpg  -t image_0000.jpg   -K camera_droidx_640x480.yml  -a NLOPT_LN_BOBYQA -w warped02.jpg
